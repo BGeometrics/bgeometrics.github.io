ffcb8c3815e3b93d514037d570a95196a213738a
bgeometrics <juliansimon@github.com>
2024-05-05 16:03:40 +0200

    daily

--- assets/js/bgeometrics.js
+++ assets/js/bgeometrics.js
@@ -197,3 +197,13 @@ function convertStringToDate(dateString) {
 
     return dateObject;
 }
+
+function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp);
+    var year = a.getFullYear();
+    var month = a.getMonth();
+    var date = a.getDate();
+    var time = date + ' ' + month + ' ' + year;
+
+    return time;
+}
