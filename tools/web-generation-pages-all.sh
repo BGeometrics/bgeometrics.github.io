457e2b9c3d40bb6f5b5b02e2239feedaeddc8313
juliansimon <juliansimon@github.com>
2024-06-27 20:47:30 +0200

    daily

--- tools/web-generation-pages-all.sh
+++ tools/web-generation-pages-all.sh
@@ -8,16 +8,15 @@ echo "#### "$(basename "$0")" ####"
 echo
 echo $(date)
 
-cd $DIR/tools
-
-./web-generation-pages-all.py
-
 cd $DIR/tools/es
 ./web-generation-pages-all.py
 
-#cd $DIR_TEMP/es
-#find . -type f -name "$DIR_TEMP/*.html" -exec sed -i 's/<html lang="en">/<html lang="es">/g' {} +
-#find . -type f -name "$DIR_TEMP/*.html" -exec sed -i 's/flag-united-kingdom flag m-0/flag-spain flag m-0/g' {} +
+cd $DIR/tools
+./web-generation-pages-all.py
+
+cd $DIR_TEMP/es
+find . -type f -name "$DIR_TEMP/*.html" -exec sed -i 's/<html lang="en">/<html lang="es">/g' {} +
+find . -type f -name "$DIR_TEMP/*.html" -exec sed -i 's/flag-united-kingdom flag m-0/flag-spain flag m-0/g' {} +
 
 cp -r $DIR_TEMP/* $DIR
 
