48556516bb16fbaaa4c76c5bdb11b01fcf19c8eb
juliansimon <juliansimon@github.com>
2024-07-06 13:59:43 +0200

    daily

--- tools/web-generation-pages-all-es.sh
+++ tools/web-generation-pages-all-es.sh
@@ -14,8 +14,7 @@ cd $DIR/tools
 ./web-generation-pages-all-es.py
 
 # Sobreescribo los gráficos que ya están traducidos
-echo "cp $DIR_PROYECT/tools/es/graphics/* $DIR/graphics/es" 
-cp $DIR_PROYECT/tools/es/graphics/* $DIR/graphics/es 
+cp $DIR_PROYECT/tools/es/graphics/* $DIR/graphics/ 
 
 cd $DIR_TEMP
 find . -type f -name "$DIR_TEMP/*.html" -exec sed -i 's/<html lang="en">/<html lang="es">/g' {} +
