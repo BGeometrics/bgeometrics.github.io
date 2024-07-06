2ff87c3cb533c4027b00fde9eab2d679b4c71c48
juliansimon <juliansimon@github.com>
2024-07-06 13:58:21 +0200

    daily

--- tools/temp.sh
+++ tools/temp.sh
@@ -9,25 +9,10 @@ echo "#### "$(basename "$0")" ####"
 echo
 echo $(date)
 
-cd $DIR/tools
-
-./web-generation-pages-all-es.py
 
 # Sobreescribo los gráficos que ya están traducidos
 echo "cp $DIR_PROYECT/tools/es/graphics/* $DIR/graphics/es" 
 cp $DIR_PROYECT/tools/es/graphics/* $DIR/graphics/es 
-
-cd $DIR_TEMP
-find . -type f -name "$DIR_TEMP/*.html" -exec sed -i 's/<html lang="en">/<html lang="es">/g' {} +
-
-cp $DIR_TEMP/* $DIR
-
-cd $DIR
-
-git pull
-git add *
-git commit -a -m"daily"
-git push
 
 echo $(date)
 echo
