7b0b0ec889b95600bfd8efb4809f923162a252d9
juliansimon <juliansimon@github.com>
2024-07-18 12:12:01 +0200

    daily

--- tools/web-generation-pages-all.sh
+++ tools/web-generation-pages-all.sh
@@ -4,18 +4,23 @@ DIR_PROYECT=$HOME/bgeometrics.github.io
 DIR_ES=$DIR_PROYECT/es
 DIR_TEMP=/tmp/web
 
+DIR_DUMP=$HOME/proyectos/bitcoin-scripts
+VENV_DIR="$DIR_DUMP/bitcoin-scripts-env"
+
 echo 
 echo "#### "$(basename "$0")" ####"
 echo
 echo $(date)
 
-cd $DIR_PROYECT/tools
-./web-generation-pages-dark.py
-./web-generation-pages-all-es.py
-./web-generation-pages-all.py
+# Virtual env python
+if [ -d "$VENV_DIR" ] && [ -f "$VENV_DIR/bin/activate" ]; then
+        source $VENV_DIR/bin/activate
+fi
 
-#cd $DIR_TEMP/es
-#find . -type f -wholename "$DIR_TEMP/*.html" -exec sed -i 's/flag-united-kingdom/flag-spain/g' {} +
+cd $DIR_PROYECT/tools
+python3 web-generation-pages-dark.py
+python3 web-generation-pages-all-es.py
+python3 web-generation-pages-all.py
 
 cp -r $DIR_PROYECT/graphics/* $DIR_ES/graphics
 cp -r $DIR_PROYECT/reports/* $DIR_ES/reports
@@ -23,7 +28,11 @@ cp -r $DIR_PROYECT/reports/* $DIR_ES/reports
 # Sobreescribo los gráficos que ya están traducidos
 cp $DIR_PROYECT/tools/es/graphics/* $DIR_ES/graphics/
 
-./web-generation-translate-graphics.py
+python3 web-generation-translate-graphics.py
+
+if [ -d "$VENV_DIR" ] && [ -f "$VENV_DIR/bin/activate" ]; then
+        deactivate
+fi
 
 cd $DIR_PROYECT/tools
 cp -r $DIR_TEMP/* $DIR_PROYECT
