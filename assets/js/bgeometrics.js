71d331f2dc830b6b475efbd306e262ee9943e40d
juliansimon <juliansimon@github.com>
2024-07-19 11:51:58 +0200

    daily

--- assets/js/bgeometrics.js
+++ assets/js/bgeometrics.js
@@ -642,18 +642,15 @@ function getMetricsShortTerm() {
 
 function tableTraffic(value, clas, term){
     if (value == 100) {
-        //clas = "class='bg-danger'";
-        clas = "style='background-color:#cc3300'";
+        clas = "class='background-alert'";
         term = "Alto";
     }
     else if (value == 50) {
-        //clas = "class='bg-warning'";
-        clas = "style='background-color:#ffcc00'";
+        clas = "class='background-warning'";
         term = "Medio";
     }
     else {
-        //cla = "class='bg-success'"
-        clas = "style='background-color:#99cc33'";
+        clas = "class='background-success'";
         term = "Bajo";
     }
     
