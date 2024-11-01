615992677bd305075cf4e405131b52d15aca3513
juliansimon <juliansimon@github.com>
2024-11-01 19:58:37 +0100

    daily

--- tools/web-generation-pages-all.py
+++ tools/web-generation-pages-all.py
@@ -476,13 +476,13 @@ concatenate_files(file1_path, file2_path, output_file_path)
 
 # Generate page dashboard_short_term_trend.html with menu
 file1_path = 'web-generation-model-dark.html'
-file2_path = 'web-generation-model-dashboard-short-term-trend-end-dark.html'
+file2_path = 'web-generation-model-dashboard-short-term-trend-end.html'
 output_file_path = '/tmp/web/dashboard_short_term_trend_dark.html'
 concatenate_files(file1_path, file2_path, output_file_path)
 
 # Generate page dashboard_demand.html with menu
 file1_path = 'web-generation-model-dark.html'
-file2_path = 'web-generation-model-dashboard-demand-end-dark.html'
+file2_path = 'web-generation-model-dashboard-demand-end.html'
 output_file_path = '/tmp/web/dashboard_demand_dark.html'
 concatenate_files(file1_path, file2_path, output_file_path)
 
