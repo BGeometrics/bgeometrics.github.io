caab11b07016d6ba21ccb115ada088c91cc5c586
juliansimon <julian.simon@gmail.com>
2025-08-13 14:07:23 +0200

    daily

--- tools/web-generation-pages-dark.py
+++ tools/web-generation-pages-dark.py
@@ -220,13 +220,13 @@ replace_text_in_directory(dest_directory, old_text, new_text, file_extension=Non
 # Copy files mode light to dark because these pages no visual good in dark
 src_file = os.path.join(dest_directory, "bitcoin_monthly_history.html")
 dest_file = os.path.join(dest_directory, "bitcoin_monthly_history_dark.html")
-shutil.copy2(src_file, dest_file)
 print(f"Copied single file: {src_file} to {dest_file}")
+shutil.copy2(src_file, dest_file)
 
 src_file = os.path.join(dest_directory, "dca.html")
 dest_file = os.path.join(dest_directory, "dca_dark.html")
-shutil.copy2(src_file, dest_file)
 print(f"Copied single file: {src_file} to {dest_file}")
+shutil.copy2(src_file, dest_file)
 
 copy_files(dest_directory, src_directory)
 
