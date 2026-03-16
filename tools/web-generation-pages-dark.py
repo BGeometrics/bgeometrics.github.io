a23301ae26dd388dad84ac4af77265e54161277d
juliansimon <juliansimon@github.com>
2026-03-16 08:47:38 +0100

    daily

--- tools/web-generation-pages-dark.py
+++ tools/web-generation-pages-dark.py
@@ -152,11 +152,11 @@ new_text = """BGeometrics_logo_write.png"""
 replace_text_in_directory(dest_directory, old_text, new_text, file_extension=None)
 
 old_text="""
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <script src="../assets/modules/accessibility.js"></script>
 """
 new_text = """
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
-    <script src="https://code.highcharts.com/themes/brand-dark.js"></script>
+    <script src="../assets/modules/accessibility.js"></script>
+    <script src="../assets/themes/brand-dark.js"></script>
     <link rel="stylesheet" type="text/css" href="../assets/css/highcharts-dark.css">
     <style>
         :root {
